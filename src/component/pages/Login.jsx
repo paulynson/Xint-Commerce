d59c610b407fa1246af550950093b05d99236a9b
Paul Abakada <33938655+paulynson@users.noreply.github.com>
2022-08-10 19:41:33 +0100

    added registration and login using firebase

--- src/component/pages/Login.jsx
+++ src/component/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     });
   };
   return (
-    <div className="flex justify-center items-center my-24 lg:my-48">
+    <div className="flex justify-center items-center h-screen ">
       <form className="bg-white shadow-md rounded px-8 pt-8 pb-8 mb-4 lg:w-[400px]">
         <div className="mb-4">
           <label
